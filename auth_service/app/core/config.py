149747eb351967cf475f30629d8cd84896c01da7
Ismail <ismll.s@mail.ru>
2026-05-12 06:19:57 +0400

    feat(security): implement JWT and tokens generation
    - add JWT configuration to config.py
    - implement tokens generation in security.py

--- auth_service/app/core/config.py
+++ auth_service/app/core/config.py
@@ -2,12 +2,18 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-
+    # DB
     DB_USER: str
     DB_PASSWORD: str
     DB_HOST: str
     DB_NAME: str
     DB_PORT: int
+
+    # JWT
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    REFRESH_TOKEN_EXPIRE_MINUTES: int
 
     model_config = SettingsConfigDict(env_file='.env', extra='ignore')
 
---
+++ auth_service/app/core/security.py
@@ -0,0 +1,41 @@
+import jwt
+from passlib.context import CryptContext
+from datetime import datetime, timedelta, timezone
+from fastapi.security import OAuth2PasswordBearer
+
+from app.core.config import settings
+
+
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='users/token')
+
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + \
+        timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({
+        'exp': expire,
+        'token_type': 'access',
+    })
+    return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+
+
+def create_refresh_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + \
+        timedelta(minutes=settings.REFRESH_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({
+        'exp': expire,
+        'token_type': 'refresh',
+    })
+    return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
