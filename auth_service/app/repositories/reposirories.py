298ed88d6421a8828948402393eb57d495603882
Ismail <ismll.s@mail.ru>
2026-05-14 13:53:02 +0400

    feat(repo): implement User and RefreshToken repositories

---
+++ auth_service/app/repositories/reposirories.py
@@ -0,0 +1,41 @@
+from collections.abc import Sequence
+
+from sqlalchemy import select, delete
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models import User, RefreshToken
+
+
+class UserRepository:
+    def __init__(self, db: AsyncSession):
+        self.db = db
+
+    async def get_by_email(self, email: str) -> User | None:
+        result = await self.db.scalar(select(User).where(User.email == email))
+        return result
+
+    async def get_by_id(self, user_id: int) -> User | None:
+        result = await self.db.scalar(select(User).where(User.id == user_id))
+        return result
+
+    async def create(self, user: User) -> User:
+        self.db.add(user)
+        await self.db.commit()
+        await self.db.refresh(user)
+        return user
+
+
+class RefreshTokenRepository:
+    def __init__(self, db: AsyncSession):
+        self.db = db
+
+    async def delete_by_user_id(self, user_id: int):
+        stmt = delete(RefreshToken).where(RefreshToken.user_id == user_id)
+        await self.db.execute(stmt)
+        await self.db.commit()
+
+    async def create(self, refresh_token: RefreshToken) -> RefreshToken:
+        self.db.add(refresh_token)
+        await self.db.commit()
+        await self.db.refresh(refresh_token)
+        return refresh_token
