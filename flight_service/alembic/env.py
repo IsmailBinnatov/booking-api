f5eb9dfd852921a4e49c0656c29a103c7e6df8b5
Ismail <ismll.s@mail.ru>
2026-05-22 15:16:42 +0400

    feat(flight_service): set up all db components
    - env variables in config file
    - set up database settings in database.py
    - connect alembic to db in env.py

--- flight_service/alembic/env.py
+++ flight_service/alembic/env.py
@@ -7,6 +7,12 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
+
+from app.core.database import Base
+from app.core.config import settings
+from app import models
+
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -20,7 +26,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -64,9 +70,11 @@ async def run_async_migrations() -> None:
     and associate a connection with the context.
 
     """
+    section = config.get_section(config.config_ini_section, {})
+    section["sqlalchemy.url"] = settings.database_url
 
     connectable = async_engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        section,
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
